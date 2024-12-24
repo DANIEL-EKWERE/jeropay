4977baa4f7895c9ae4f0aa6e45c7df12f3e27b67
DANIEL-EKWERE <ekweredaniel8@gmail.com>
2024-12-24 17:32:21 +0100

    updated cors1

--- DATABANK/settings.py
+++ DATABANK/settings.py
@@ -179,7 +179,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     # 'https://microepay.com',
     # 'http://localhost:8000',
-    'https://daniel-ekwere.github.io/jeropaywebapp/',
+    'https://daniel-ekwere.github.io',
     
 ]
 
