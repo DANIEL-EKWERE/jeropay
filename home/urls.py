8bf038489054f839c8f7223c1f53a60dad335d21
DANIEL-EKWERE <ekweredaniel8@gmail.com>
2025-09-04 14:35:07 +0100

    changing the frontend

--- home/urls.py
+++ home/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 
 # views
-from .views import HomeView, DownloadView
+from .views import AboutView, HomeView, DownloadView, PrivacyPolicyView, TermsAndConditionView
 
 app_name='home'
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('about/', AboutView.as_view(), name='about'),
+    path('privacy-policy/', PrivacyPolicyView.as_view(), name='privacy-policy'),
+    path('terms-and-condition/', TermsAndConditionView.as_view(), name='terms-and-condition'),
     path('download-app/', DownloadView.as_view(), name='download-app'),
 ]
--- home/views.py
+++ home/views.py
@@ -6,6 +6,15 @@ from .models import AppDownload
 class HomeView(TemplateView):
     template_name = 'base.html'
 
+class AboutView(TemplateView):
+    template_name = 'about.html'
+
+class PrivacyPolicyView(TemplateView):
+    template_name = 'privacy-policy.html'
+
+class TermsAndConditionView(TemplateView):
+    template_name = 'terms-and-condition.html'
+
 class DownloadView(ListView):
     template_name = 'download-app.html'
     queryset = AppDownload.objects.all().order_by('-upload_date')
